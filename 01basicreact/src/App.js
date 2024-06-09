91f4c38f78e0f9e41029b82841f83504c5ac2acc
vanshm2703 <vanshm2703@gmail.com>
2024-06-09 22:21:05 +0530

    new folders

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <div> 
+   <h1>classic react app</h1>
+    <Youtube />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube(){
+    return(
+        <h1>Youtube React App</h1>
+    )
+}
+
+export default Youtube;
