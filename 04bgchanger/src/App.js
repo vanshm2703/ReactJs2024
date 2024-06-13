3214770494f355bd8add7be621681704b3b370df
vanshm2703 <vanshm2703@gmail.com>
2024-06-13 17:07:11 +0530

    new update

---
+++ 04bgchanger/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [color,setColor]=useState('olive')
+  // function changeColor(color) {
+  //   setColor(color)
+
+  // }
+
+  return (
+    <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
+         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+            <div className='fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl;'>
+              <button 
+                onClick={()=>setColor('red')} 
+               className='outline-none px-4 py-1
+               rounded-full shadow-lg text-black'style={{backgroundColor:'red'}} >red</button>
+              <button
+                 onClick={()=>setColor('green')}
+               className='outline-none px-4 py-1 rounded-full shadow-lg text-black' style={{backgroundColor:'green'}} >green</button>
+            </div>
+         </div>
+    </div>
+  );
+}
+
+export default App;
